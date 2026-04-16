15b34f50973db9c5821729cfecd2ada9401d6d2c
yguidella <y.guidella8@outlook.com>
2026-04-16 12:28:13 -0300

    feat: Crud Completo Categoria

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CategoriaModule } from './categoria/categoria.module';
+import { Categoria } from './categoria/entities/categoria.entity';
 
 @Module({
   imports: [
@@ -12,9 +14,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       username: 'root',
       password: 'root',
       database: 'db_farmacia',
-      entities: [],
+      entities: [Categoria],
       synchronize: true,
-    })
+    }),
+    CategoriaModule
   ],
   controllers: [AppController],
   providers: [AppService],
